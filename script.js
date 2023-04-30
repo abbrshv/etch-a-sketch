f67ad1025787dd7f5110e0106475a4c0f4dc6444
abbrshv <a.bobryshov@proton.me>
2023-04-30 22:46:22 +0300

    Modify index.html, script.js, style.css
    Add additional styling

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const root = document.querySelector(":root");
+const headerTitle = document.querySelector(".header__title");
 const sketchpad = document.querySelector(".sketchpad");
 const colorPicker = document.querySelector(".color-picker");
 const sizeSlider = document.querySelector(".size-slider");
@@ -5,43 +7,56 @@ const sizeValue = document.querySelector(".size-value");
 const btnReset = document.querySelector(".btn-reset");
 const btnEraser = document.querySelector(".btn-eraser");
 const btnRainbow = document.querySelector(".btn-rainbow");
+const colorInput = document.getElementById("color");
 
 let drag = false;
-let color = "#000000";
 let size = 16;
-let eraserToggled = false;
-let rainbowToggled = false;
+let color = generateRandomColor();
+
+colorInput.value = color;
+root.style.setProperty("--color", `${color}`);
 
 document.addEventListener("pointerdown", () => (drag = true));
 document.addEventListener("pointerup", () => (drag = false));
-colorPicker.addEventListener("input", (e) => (color = e.target.value));
+colorPicker.addEventListener("input", (e) => {
+  color = e.target.value;
+  root.style.setProperty("--color", `${e.target.value}`);
+});
 sizeSlider.addEventListener("input", (e) => {
   size = e.target.value;
   generateGrid(size);
 });
-btnEraser.addEventListener("click", () => (eraserToggled = !eraserToggled));
-btnRainbow.addEventListener("click", () => (rainbowToggled = !rainbowToggled));
+btnEraser.addEventListener("click", handleButton);
+btnRainbow.addEventListener("click", handleButton);
 btnReset.addEventListener("click", () => generateGrid(size));
+
+function handleButton(event) {
+  event.target.classList.toggle("active");
+
+  if (event.target === btnRainbow) {
+    event.target.classList.toggle("rainbow-mode");
+    headerTitle.classList.toggle("rainbow-mode");
+  }
+}
 
 function generateRandomColor() {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 function paint(block) {
-  block.style.backgroundColor = rainbowToggled
+  block.style.backgroundColor = btnRainbow.classList.contains("active")
     ? generateRandomColor()
-    : eraserToggled
-    ? "#ffffff"
+    : btnEraser.classList.contains("active")
+    ? "white"
     : color;
 }
 
 function generateGrid(size) {
   sketchpad.innerHTML = "";
 
-  sketchpad.style.border = "1px solid black"; //move to CSS
   sketchpad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   sketchpad.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  sizeValue.textContent = `Size: ${size} x ${size}`;
+  sizeValue.textContent = `${size} x ${size}`;
 
   for (let i = 0; i < size ** 2; i++) {
     let block = document.createElement("div");
